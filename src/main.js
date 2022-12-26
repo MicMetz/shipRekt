0b5f0b47e82f4b7c54638dbd975cfd6b7fac30a3
MicMetz <mime9599@colorado.edu>
2022-12-26 05:25:55 -0700

    Player Animation system improved

--- src/main.js
+++ src/main.js
@@ -2,10 +2,8 @@
  * @author MicMetzger /
  */
 
-    // import world from './core/World.js';
 
 
-    // const splashScreen = document.getElementById("splashScreen");
 const startButton = document.getElementById('start-button');
 import('./core/World.js').then((game) => {
     startButton.onclick = function () {
